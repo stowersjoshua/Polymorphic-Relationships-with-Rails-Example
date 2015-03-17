3ce2d5c72d8a1d23caed9d4a92de8bfbd75d27e2
pascbjumper2 <stowers.joshua@live.com>
2015-03-17 00:49:41 -0500

    Users can now have usernames. Check out the application controller to see what I did.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ActiveRecord::Base
+  validates :username, :uniqueness => true
+
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+
+  # I guess Rails 4.1 doesn't allow this.. =/
+  #attr_accessible :email, :password, :password_confirmation, :remember_me, :username
 end
